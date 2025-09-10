adf389eac3d0b14e88b0964b0feef6e909da9199
Twinkle <twinkledevwanshi93@gmail.com>
2025-09-09 21:23:46 -0700

    fixing in db

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS reservation_items;
 DROP TABLE IF EXISTS reservations;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS books;
@@ -5,11 +6,11 @@ DROP TABLE IF EXISTS books;
 
 CREATE TABLE users(
     id serial PRIMARY KEY,
-    firstname text NOT NULL,
-    lastname text NOT NULL, 
+    first_name text NOT NULL,
+    last_name text NOT NULL, 
     email text NOT NULL,
-    password text NOT NULL,
-    UNIQUE (email)
+    password text NOT NULL
+ 
 );
 
 CREATE TABLE books(
@@ -17,13 +18,20 @@ CREATE TABLE books(
     title text NOT NULL,
     author text NOT NULL, 
     description text NOT NULL,
-    coverImage text NOT NULL, 
+    cover_image text NOT NULL, 
     available boolean DEFAULT true
 );
 
 CREATE TABLE reservations(
         id serial PRIMARY KEY,
-        user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-        book_id integer NOT NULL REFERENCES books(id) on DELETE CASCADE,
-        UNIQUE (user_id, book_id)
+        check_in DATE NOT NULL,
+        check_out DATE,
+        user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
+     
+);
+
+CREATE TABLE reservation_items (
+  id serial PRIMARY KEY,
+  reservation_id INTEGER NOT NULL REFERENCES reservations (id) ON DELETE CASCADE,
+  book_id INTEGER UNIQUE NOT NULL REFERENCES books (id) ON DELETE CASCADE
 );
